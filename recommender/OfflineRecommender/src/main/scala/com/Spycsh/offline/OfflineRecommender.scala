4bb243b0632d881777bf0e9d680741c80630a721
Spycsh <757407490@qq.com>
2020-12-20 17:37:46 +0100

    offline recommender

---
+++ recommender/OfflineRecommender/src/main/scala/com/Spycsh/offline/OfflineRecommender.scala
@@ -0,0 +1,130 @@
+package com.Spycsh.offline
+
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.recommendation.{ALS, Rating}
+import org.apache.spark.sql.SparkSession
+import org.jblas.DoubleMatrix
+
+// LFM (latent factor model) based on score, only Rating is needed
+case class MovieRating(uid: Int, mid: Int, score: Double, timestamp: Int )
+
+case class MongoConfig(uri:String, db:String)
+
+// define a basic recommendation object
+case class Recommendation( mid: Int, score: Double )
+
+// define the recommendation list for users
+case class UserRecs( uid: Int, recs: Seq[Recommendation] )
+
+// define the similar movie list based on LFM movie feature vectors
+case class MovieRecs( mid: Int, recs: Seq[Recommendation] )
+
+
+object OfflineRecommender {
+  val MONGODB_RATING_COLLECTION = "Rating"
+
+  val USER_RECS = "UserRecs"
+  val MOVIE_RECS = "MovieRecs"
+
+  val USER_MAX_RECOMMENDATION = 20
+
+  def main(args: Array[String]): Unit = {
+    val config = Map(
+      "spark.cores" -> "local[*]",
+      "mongo.uri" -> "mongodb://localhost:27017/recommender",
+      "mongo.db" -> "recommender"
+    )
+
+    val sparkConf = new SparkConf().setMaster(config("spark.cores")).setAppName("OfflineRecommender")
+
+    val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+
+    import spark.implicits._
+
+    implicit val mongoConfig = MongoConfig(config("mongo.uri"), config("mongo.db"))
+
+
+    // read data from Rating
+    val ratingRDD = spark.read
+      .option("uri", mongoConfig.uri)
+      .option("collection", MONGODB_RATING_COLLECTION)
+      .format("com.mongodb.spark.sql")
+      .load()
+      .as[MovieRating]
+      .rdd
+      .map( rating => ( rating.uid, rating.mid, rating.score ) )    // convert to rdd without timestamp
+      .cache()
+
+    // extract all the uid and mid from rating data, and remove duplicated ones
+    val userRDD = ratingRDD.map(_._1).distinct()
+    val movieRDD = ratingRDD.map(_._2).distinct()
+
+    // train LFM model
+    val trainData = ratingRDD.map( x => Rating(x._1, x._2, x._3) )
+
+    val (rank, iterations, lambda) = (200, 5, 0.1)
+    val model = ALS.train(trainData, rank, iterations, lambda)
+
+    // Based on the latent features of users and movies, calculate the prediction rating scores
+    // to obtain the recommendation list of a user
+    // calculate the cartesian product of uid and mid, obtain a matrix with no rating scores
+    val userMovies = userRDD.cartesian(movieRDD)
+
+    val preRatings = model.predict(userMovies)
+
+    val userRecs = preRatings
+      .filter(_.rating > 0)
+      .map(rating => ( rating.user, (rating.product, rating.rating) ) )   // uid (mid rating)
+      .groupByKey()
+      .map{
+        case (uid, recs) => UserRecs( uid, recs.toList.sortWith(_._2>_._2).take(USER_MAX_RECOMMENDATION).map(x=>Recommendation(x._1, x._2)) )
+      }
+      .toDF()
+
+    userRecs.write
+      .option("uri", mongoConfig.uri)
+      .option("collection", USER_RECS)
+      .mode("overwrite")
+      .format("com.mongodb.spark.sql")
+      .save()
+
+    // calculate correlation matrix based on latent features of movies
+    val movieFeatures = model.productFeatures.map{
+      case (mid, features) => (mid, new DoubleMatrix(features))
+    }
+
+    // cartesian product of every two of the movies
+    val movieRecs = movieFeatures.cartesian(movieFeatures)
+      .filter{
+        // filter out the record that match itself
+        case (a, b) => a._1 != b._1
+      }
+      .map{
+        case (a, b) => {
+          val simScore = this.consinSim(a._2, b._2)
+          ( a._1, ( b._1, simScore ) )
+        }
+      }
+      .filter(_._2._2 > 0.6)    // filter out the records whose similarity > 0.6
+      .groupByKey()
+      .map{
+        case (mid, items) => MovieRecs( mid, items.toList.sortWith(_._2 > _._2).map(x => Recommendation(x._1, x._2)) )
+      }
+      .toDF()
+    movieRecs.write
+      .option("uri", mongoConfig.uri)
+      .option("collection", MOVIE_RECS)
+      .mode("overwrite")
+      .format("com.mongodb.spark.sql")
+      .save()
+
+    spark.stop()
+  }
+
+  // calculate the cosine of two feature vectors
+  def consinSim(movie1: DoubleMatrix, movie2: DoubleMatrix):Double ={
+    movie1.dot(movie2) / ( movie1.norm2() * movie2.norm2() )
+  }
+
+}
+
